4ca9dcb2207235ca1f8fe99e4930bb55ef17c616
Ng <dostoyevsky.ng@outlook.com>
2025-06-23 01:26:14 +0800

    first commit

---
+++ Inc/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define PI 3.14159265358979323846
+#endif
---
+++ Src/main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << PI << std::endl;
+  for (int i = 0; i < 10; ++i) {
+    std::cout << "Counter: " << i << std::endl;
+  }
+  return 0;
+}
