008edf9d2899de6e67988715852019edad30288e
Ng <dostoyevsky.ng@outlook.com>
2025-06-23 10:31:45 +0800

    updateCtestExample

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,9 +2,11 @@
 
 #include <iostream>
 
+#include "sum.h"
+
 int main() {
   std::cout << "Hello, World!" << std::endl;
-  std::cout << PI << std::endl;
+  std::cout << PI << std::endl << sum(1, 2) << std::endl;
   for (int i = 0; i < 10; ++i) {
     std::cout << "Counter: " << i << std::endl;
   }
--- Test/test.cpp
+++ Test/test.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-#include "main.h"
-int main() {
-  std::cout << PI << std::endl;
-  return 0;
+#include "sum.h"  // 确保 sum.h 在 Inc 目录中
+
+// 定义一个名为 "SumTest" 的测试套件，以及一个名为 "Basic" 的测试用例
+TEST(SumTest, Basic) {
+  EXPECT_EQ(sum(1, 2), 3);
+  EXPECT_EQ(sum(-1, 1), 0);
 }
